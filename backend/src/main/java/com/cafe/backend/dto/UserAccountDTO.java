5026eb828952e707d6900ac634767252d991597d
Matrix2121 <vasko10.vs38@gmail.com>
2025-03-18 10:45:38 +0200

    User Control Implementation

--- backend/src/main/java/com/cafe/backend/dto/UserAccountDTO.java
+++ backend/src/main/java/com/cafe/backend/dto/UserAccountDTO.java
@@ -10,5 +10,4 @@ public record UserAccountDTO(
         String username,
         Set<RoleDTO> role,
         Set<OrderDTO> orders
-) {
-}
+) {}
---
+++ backend/src/main/java/com/cafe/backend/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.cafe.backend.repository;
+
+import com.cafe.backend.entity.account.UserEntity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+}
