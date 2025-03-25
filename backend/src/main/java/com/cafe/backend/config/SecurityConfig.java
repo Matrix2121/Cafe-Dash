cf0d1f464d2dd4cde46a3de59360959317030839
Angel Stoynov <an.stoynov@gmail.com>
2025-03-25 12:24:45 +0200

    change security config

--- backend/src/main/java/com/cafe/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/cafe/backend/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.cafe.backend.config;
 
+import com.cafe.backend.filter.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,12 +14,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.cafe.backend.filter.JwtAuthenticationFilter;
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    
+
     @Autowired
     private JwtAuthenticationFilter jwtAuthenticationFilter;
 
@@ -29,31 +28,33 @@ public class SecurityConfig {
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
-         return authConfig.getAuthenticationManager();
+        return authConfig.getAuthenticationManager();
     }
-    
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-         http
-            .csrf().disable()
-            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and()
-            .authorizeHttpRequests(authorize -> authorize
-            		.requestMatchers(
-            				"/**", // will be removed
-            				"/api/auth/**",
-                            "/v2/api-docs/**",
-                            "/v3/api-docs/**",
-                            "/swagger-resources/**",
-                            "/swagger-ui/**",
-                            "/webjars/**"
+        http
+                .cors()
+                .and()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(
+                                "/**", // will be removed
+                                "/api/auth/**",
+                                "/v2/api-docs/**",
+                                "/v3/api-docs/**",
+                                "/swagger-resources/**",
+                                "/swagger-ui/**",
+                                "/webjars/**"
                         ).permitAll()
-                .anyRequest().authenticated()
-            )
-            .httpBasic().disable();
+                        .anyRequest().authenticated()
+                )
+                .httpBasic().disable();
 
-         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
-         return http.build();
+        return http.build();
     }
 }
